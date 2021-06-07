72eae2fd70a428457451e9c51058ead4189a0a02
JimmyVanetta <j_vanetta@zoominternet.net>
2021-06-07 16:51:26 -0400

    first commit

---
+++ src/app/components/CalendarEntry.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="calendar-entry" class="">
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'CalendarEntry',
+    }
+</script>
+
+<style lang="scss" scoped>
+    #calendar-entry {
+        
+    }
+</style>
---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+//import Vue from 'vue'
+import { seedData } from './seed.js'
+
+export const store = {
+    state: {
+        seedData
+    },
+    getActiveDay() {
+        
+    }
+}
