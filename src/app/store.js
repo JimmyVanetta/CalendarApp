ea4c8af3504dead15d322ae56cb66ed19d2abe9d
JimmyVanetta <j_vanetta@zoominternet.net>
2021-06-09 17:05:05 -0400

    adding todays work

--- src/app/store.js
+++ src/app/store.js
@@ -5,6 +5,7 @@ export const store = {
     state: {
         seedData
     },
+    // DAY //
     getActiveDay() {
         return this.state.seedData.find((day) => day.active)
     },
@@ -13,6 +14,7 @@ export const store = {
             dayObj.id === dayId ? dayObj.active = true : dayObj.active = false
         })
     }, 
+    // EVENT //
     submitEvent(eventDetails) {
         const activeDay = this.getActiveDay()
         activeDay.events.push({ "details": eventDetails, "edit": false })
@@ -23,7 +25,18 @@ export const store = {
         const eventObj = dayObj.events.find(event => event.details === eventDetails)
         eventObj.edit = true
     },
+    updateEvent(dayId, originalEventDetails, newEventDetails) {
+        const dayObj = this.state.seedData.find(day => day.id === dayId)
+        const eventObj = dayObj.events.find(event => event.details === originalEventDetails)
+        eventObj.details = newEventDetails
+        eventObj.edit = false
+    },
     resetEditOfAllEvents() {
         this.state.seedData.map((dayObj) => dayObj.events.map((event) => event.edit = false))
+    },
+    // HELPERS //
+    getEventObj(dayId, eventDetails) {
+        const dayObj = this.state.seedData.find(day => day.id === dayId)
+        return dayObj.events.find(event => event.details === eventDetails)
     }
 }
